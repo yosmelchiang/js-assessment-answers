ee4a7a93176de60cfaf7e19949ae5cf19cab4944
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-28 18:31:47 +0000

    solve multiple array tests

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 My solutions for the JS-assessment by [rmurphey](https://github.com/rmurphey)
 
-Feel free to fork this repo, if you prefer a blank assessment for [this](https://github.com/rmurphey/js-assessment) instead
+Feel free to fork this repo, if you prefer a blank assessment fork [this](https://github.com/rmurphey/js-assessment) instead
--- app/arrays.js
+++ app/arrays.js
@@ -1,15 +1,15 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
+  indexOf: function (arr, item) {
     return arr.indexOf(item)
   },
 
-  sum: function(arr) {
+  sum: function (arr) {
     let result = 0;
 
     // For loop
-    for(let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
       result += arr[i]
     }
 
@@ -19,67 +19,81 @@ exports.arraysAnswers = {
     return result;
   },
 
-  remove: function(arr, item) {
+  remove: function (arr, item) {
     const newArr = [];
 
     // For loop
-    for(let i = 0; i < arr.length; i++) {
-      if(arr[i] !== item) newArr.push(arr[i])
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== item) newArr.push(arr[i])
     }
 
     // Array.prototype.filter()
     // return arr.filter(i => i !== item)
-    
+
     return newArr;
   },
 
-  removeWithoutCopy: function(arr, item) {
-    
+  removeWithoutCopy: function (arr, item) {
+
     // We iterate backwards and and remove items backwards to avoid shifting indexes in the original array
-    for(let i = arr.length; i >= 0; i--) {
-      if(arr[i] === item) arr.splice(i, 1)
+    for (let i = arr.length; i >= 0; i--) {
+      if (arr[i] === item) arr.splice(i, 1)
     }
     return arr
   },
 
-  append: function(arr, item) {
+  append: function (arr, item) {
     arr.push(item)
     return arr
   },
 
-  truncate: function(arr) {
-
+  truncate: function (arr) {
+    arr.pop()
+    return arr
   },
 
-  prepend: function(arr, item) {
-
+  prepend: function (arr, item) {
+    arr.unshift(item)
+    return arr;
   },
 
-  curtail: function(arr) {
-
+  curtail: function (arr) {
+    arr.shift()
+    return arr;
   },
 
-  concat: function(arr1, arr2) {
+  concat: function (arr1, arr2) {
 
+    // Array.prototype.concat()
+    // return arr1.concat(arr2)
+
+    return [...arr1, ...arr2]
   },
 
-  insert: function(arr, item, index) {
+  insert: function (arr, item, index) {
+    const newArr = [];
 
+    for (let i = 0; i < arr.length; i++) {
+      if (i === index) newArr.push(item)
+      newArr.push(arr[i])
+    }
+
+    return newArr;
   },
 
-  count: function(arr, item) {
-
-  },
-
-  duplicates: function(arr) {
-
-  },
-
-  square: function(arr) {
-
-  },
-
-  findAllOccurrences: function(arr, target) {
+  count: function (arr, item) {
     
+  },
+
+  duplicates: function (arr) {
+
+  },
+
+  square: function (arr) {
+
+  },
+
+  findAllOccurrences: function (arr, target) {
+
   }
 };
