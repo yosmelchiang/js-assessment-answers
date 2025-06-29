e891a0d33d8122b109e88b2fa6a76522a5b38aa3
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-29 21:39:56 +0200

    solve count.js

--- app/count.js
+++ app/count.js
@@ -2,6 +2,29 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    // Define i as start value
+    let i = start;
+    console.log(i) // immediately log i
 
+    // Create an interval that will log an incremented i every 100ms
+    // For as long as i is less or equal to end
+    // clear the interval from the inside if the condition is not met
+    const interval = setInterval(() => {
+      i++;
+
+      if (i <= end) {
+        console.log(i)
+      } else {
+        clearInterval(interval)
+      }
+
+    }, 100)
+
+    // return an object with a cancel method
+    return {
+      cancel: () => {
+        clearInterval(interval)
+      }
+    }
   }
 };
