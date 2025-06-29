a5d69089ac210b85218b31be402442c090c06b22
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-29 21:51:43 +0200

    solve flowControl

--- app/flowControl.js
+++ app/flowControl.js
@@ -2,6 +2,14 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.flowControlAnswers = {
   fizzBuzz: function(num) {
+    console.log(num)
+    if(!num || isNaN(num)) return false
+
+    if(num % 3 === 0 && num % 5 === 0) return 'fizzbuzz'
+    if(num % 3 === 0) return 'fizz'
+    if(num % 5 === 0) return 'buzz'
+
+    return num
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
