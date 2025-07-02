39206c991963d8d853637f0a2291035cd4c5126a
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-07-02 23:32:08 +0200

    solve numbers

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,28 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    return (num >> (bit - 1)) & 1;
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2)
   },
 
   convertToBinary: function(num) {
+    const converted = num.toString(2);
+    let result = ''
 
+    if(converted.length !== 8) result += 0;
+
+    for(let i = 0; i < converted.length; i++) {
+      result += converted[i]
+    }
+
+    return result
   },
 
   multiply: function(a, b) {
-
+    const result = a * b
+    return parseFloat(result.toFixed(10)) // Using parseFloat to convert it back to a number and removing trailing errors
   }
 };
