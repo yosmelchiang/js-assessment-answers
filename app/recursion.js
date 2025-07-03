f528ca7f8ae907859adf95d7ae6dbd920c121fbb
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-07-03 15:29:33 +0200

    solve recursion/listFiles

--- app/recursion.js
+++ app/recursion.js
@@ -1,19 +1,64 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
-  listFiles: function(data, dirName) {
+  listFiles: function (data, dirName) {
+    const result = [];
+    let collectFromThisDir;
+
+    // If dirName is not provided, start collecting right away
+    // If dirName is provided, dont collect, wait until we are on the right dir
+    if (!dirName) {
+      collectFromThisDir = true;
+    } else {
+      collectFromThisDir = false
+    }
+
+    function recurse(data) {
+      const { dir } = data
+      const { files } = data
+
+      // Given that dirName is provided, this will be ignored for any other recurson until we meet the right dir
+      // Check if we are on the dirName we are supposed to collect from, given that dirName is provided
+      if (dir === dirName) {
+        collectFromThisDir = true
+      }
+
+      // Files logic
+      // Parse through the files
+      // If its a string, push it to result
+      // If its an array, call recursion again
+      // Subdir logic
+      // Save only the files in specified subdir (dirName)
+
+      for (let i = 0; i < files.length; i++) {
+        const item = files[i]
+        if (typeof item === 'string') {
+          if (collectFromThisDir) result.push(item)
+        } else {
+          recurse(item)
+        }
+      }
+
+      // By the end of the loop
+      // If we are done collecting from the specified dirName, set collectFromThisDir to false so we dont collect more
+      if (dir === dirName) {
+        collectFromThisDir = false
+      }
+    }
+
+    recurse(data)
+    return result;
+  },
+
+  permute: function (arr) {
 
   },
 
-  permute: function(arr) {
+  fibonacci: function (n) {
 
   },
 
-  fibonacci: function(n) {
-
-  },
-
-  validParentheses: function(n) {
+  validParentheses: function (n) {
 
   }
 };
