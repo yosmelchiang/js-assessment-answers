2bcb84e1b48e0f945575a3484deacbae598093ac
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-07-03 15:56:14 +0200

    solve recursiong/fibonacci

--- app/recursion.js
+++ app/recursion.js
@@ -51,11 +51,13 @@ exports.recursionAnswers = {
   },
 
   permute: function (arr) {
-
+    // console.log(arr)
   },
 
   fibonacci: function (n) {
+    if(n === 0 || n === 1) return n
 
+    return this.fibonacci(n - 1) + this.fibonacci(n - 2)
   },
 
   validParentheses: function (n) {
